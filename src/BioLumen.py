7c9ccf7c5d03f0fc1d08701bebef8bc3e3139289
Joseph Herbst <herbs070@umn.edu>
2019-06-19 22:35:23 -0500

    Update BioLumen.py

--- src/BioLumen.py
+++ src/BioLumen.py
@@ -1,50 +1,87 @@
 import time
+import datetime
 import csv
 import RPi.GPIO as io
 from pumps import *
 from temp import *
+#Database/website
+from firebase import firebase
 #pH configuration
 import Adafruit_MCP3008
 mcp = Adafruit_MCP3008.MCP3008(18, 25, 23, 24)
+firebase = firebase.FirebaseApplication('https://biolumenreactor.firebaseio.com')
+
 
 def start_up():
+    firebase.put('','/running',1)
     io.setwarnings(False)
     io.setup(22, io.IN, pull_up_down=io.PUD_DOWN)
-    io.setup(10,io.OUT)
+    io.setup(10,io.OUT) #LED light
+    io.output(10, False)
     initiatePumps()
+    ready = False
+    while !ready and read_temp() > 26.0 and read_temp() < 29.0:
+        for j in range(5):
+             total+=mcp.read_adc(0)-300 ##-300 to adjust
+             time.sleep(1)
+        ##Convert to pH
+        cur_pH = (-(((total/5) * 0.013671875) - 7)+7)
+        ##Pump base or not
+        if cur_pH > 8.0:
+            pumpBase(0.5)
+            ready = False
+        elif cur_pH < 6.0:
+            pumpAcid(0.5)
+            ready = False
+        elif cur_pH <= 7.5 and cur_pH >= 6.5:
+            turnOff()
+            ready = True
+
 
 def main():
-    start_time = time.time()  ##Time the program started
+    end_time = datetime.datetime.now() + datetime.timedelta(hours=8)
     with open('data_file.csv', mode='w') as data_file:
         ##Add column headings to the csv
         data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         data_writer.writerow(['Hour', 'Min', 'Second', 'Total Seconds', 'Temp', 'pH'])
-        i=0
-        while i<5:
-            ##Gather time
-            cur_time = time.localtime(time.time())
-            total_seconds = time.time()-start_time
+        while (end_time - datetime.datetime.now()).seconds > 0: #while there is still time left
             ##Gather temp
             cur_temp = read_temp()
             ##Gather ph average over a set time
-	    total = 0 
-            for j in range(5):
-                total+=mcp.read_adc(0)-300
-                time.sleep(0.2)
-            cur_pH = (-(((total/5) * 0.013671875) - 7)+7)
-            if cur_pH > 8.0:
-                pumpBase(0.5)
-            elif cur_pH < 6.0:
-                pumpAcid(0.5)
-            elif cur_pH <= 7.5 and cur_pH >= 6.5:
-                turnOff()
+	    total = 0
+            for i in range(12):  ##Run for a minute    
+                for j in range(5):
+                    total+=mcp.read_adc(0)-300 ##-300 to adjust
+                    time.sleep(1)
+
+                ##Convert to pH
+                cur_pH = (-(((total/5) * 0.013671875) - 7)+7)
+
+                ##Pump base or not
+                if cur_pH > 8.0:
+                    pumpBase(0.5)
+                elif cur_pH < 6.0:
+                    pumpAcid(0.5)
+                elif cur_pH <= 7.5 and cur_pH >= 6.5:
+                    turnOff()
                 
-            ##Print realtime stats
-            print("Time:", cur_time.tm_hour, " ", cur_time.tm_min, " ", cur_time.tm_sec, " ", cur_temp, " ", cur_pH)
+            ##Print realtime stats per min
+            print("Time:", now.seconds//3600, " ", (now.seconds//60)%60, " ", (now.seconds)%60, " ", cur_temp, " ", cur_pH)
 
+            ##Post to website
+            now = end_time-datetime.datetime.now()
+            firebase.put('','/hours',now.seconds//3600)
+            firebase.put('','/min',(now.seconds//60)%60)
+            firebase.put('','/seconds',(now.seconds)%60)
+            firebase.put('','/temp',getRandTemp())
+            firebase.put('','/ph',getRandpH())
+        
+            
             ##Write the data to the csv
-            data_writer.writerow([cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec, total_seconds, cur_temp, cur_pH])
-
+            data_writer.writerow([now.seconds//3600, (now.seconds//60)%60, now.seconds%60, now.seconds, cur_temp, cur_pH])
+    close(data_file)
+    print("Process Finished")
+    firebase.put('','/running',0)
 
 if __name__ == "__main__":
     try:
@@ -54,6 +91,7 @@ if __name__ == "__main__":
         io.wait_for_edge(22, io.RISING)
         io.output(10, False)
         main()
-    except KeyboardInterrupt:  
+    except KeyboardInterrupt:
+        firebase.put('','/running',-1)
         io.cleanup()       # clean up GPIO on CTRL+C exit  
-    io.cleanup()           # clean up GPIO on normal exit  
+io.cleanup() # clean up GPIO on normal exit 
