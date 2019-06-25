e0124ce6af74006cea0471f143fa13bee51c0fdc
Joseph Herbst <herbs070@umn.edu>
2019-06-25 18:44:33 -0500

    Add files via upload

---
+++ WebPage/Contact_files/theme.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(jQuery) {
+
+    "use strict";
+
+    jQuery('#primary-menu').slicknav({
+        appendTo        : '.site-branding',
+        label           : '',
+        allowParentLinks: true
+    });
+
+});
